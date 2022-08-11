989c4cd7e3fb06d84d35e4e1584c661b20ffe2a5
Pavel Chekalin <grokki91@gmail.com>
2022-08-11 23:16:28 +0300

    first homework done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let discriminant = b**2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr = [-b / (2 * a)];
+  } else if (discriminant > 0) {
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+  }
+
   return arr; // array
 }
 
