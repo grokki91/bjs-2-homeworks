3ca736e3dbcc22e566b2512729c52668d6d982eb
Pavel Chekalin <grokki91@gmail.com>
2022-08-14 02:32:40 +0300

    Homework 2 - Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,65 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  sum = 0;
+  min = arr[0];
+  max = arr[0];
 
-  // Ваш код
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+    sum += arr[i]
+  }
+
+  avg = +(sum / arr.length).toFixed(2)
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = arrOfArr[0][0];
 
-  // Ваш кода
-  // for ...
-  
+  for (let j = 0; j < arrOfArr.length; j++) {
+    let arr = arrOfArr[j];
+    if (func(arr) > max) {
+      max = func(arr);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = arr[0];
+  let min = arr[0];
+  let result;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } 
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  result = max - min;
+  Math.abs(result);
+
+  return result;
 }
