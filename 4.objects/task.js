7bca15fc4f065c3f98fa9de768a903dd6ff60e4d
Pavel Chekalin <grokki91@gmail.com>
2022-08-25 01:15:08 +0300

    hw-4 objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  return (this.subject = subjectName);
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+  return this.marks;
+};
+
+Student.prototype.addMarks = function (...marks) {
+  return (this.marks = marks);
+};
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((acc, elem) => acc + elem) / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  return (this.excluded = reason);
+};
+
+let ivan = new Student("Ivan", "male", 19);
+let zina = new Student("Zina", "female", 22);
