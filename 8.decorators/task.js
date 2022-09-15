f5d07d5d1e4d0949a7f5dece650a915c74eaae06
Pavel Chekalin <grokki91@gmail.com>
2022-09-15 20:47:40 +0300

    hw-8 decorators task1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,49 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  let cacheObj = {};
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let cacheIndex = cache.findIndex(elem => {
+      if (elem.hash.every((element, index) => element === args[index])) 
+        return elem;
+      }
+    )
+    if (cacheIndex === -1) {
+      let result = func(...args);
+      cacheObj = {
+        hash : args,
+        value : result
+      }
+
+      cache.push(cacheObj);
+      if (cache.length > 5) {
+        cache.shift(cacheObj);
+      }
+      return `Вычисляем: ${result}`;
+    } else {
+      return `Из кэша: ${cache[cacheIndex].value}`;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+// function debounceDecoratorNew(func, delay) {
+//   let count = 0;
+//   let timeoutId = null;
+//   function wrapper(...args) {
+//     wrapper.allCount.push(args);
+//     if (timeoutId) {
+//       console.log(timeoutId);
+//       clearTimeout(timeoutId);
+//     }
+//     timeoutId = setTimeout(() => {
+//       timeoutId = null;
+//       console.log(func(...args));
+//     }, delay);
+
+//   }
+//   wrapper.allCount = [];
+
+//   return wrapper;
+// }
